0fe1c29bd04ce6e06a8ed11799a431b7d62e1125
Arun Maurya <mauryaarun@gmail.com>
2023-10-13 11:42:26 +0530

    rhel-hardeing.sh updated commit

--- redhat/rhel-hardening.sh
+++ redhat/rhel-hardening.sh
@@ -1,66 +1,82 @@
 #!/bin/bash
 
-# Ensure the script is run as root
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run this script as root."
-  exit 1
+# Check if the system is running Red Hat Enterprise Linux (RHEL)
+if [ -f /etc/redhat-release ]; then
+    echo "Red Hat Linux detected. Starting hardening process..."
+else
+    echo "This script is intended for Red Hat Linux. Exiting..."
+    exit 1
 fi
 
-# Enable the firewall and configure it
-systemctl enable firewalld
-systemctl start firewalld
-firewall-cmd --permanent --zone=public --add-service=ssh
-firewall-cmd --reload
+# Update packages
+yum update -y
 
-# Disable unused services
-systemctl disable postfix
-systemctl stop postfix
-systemctl disable rpcbind
-systemctl stop rpcbind
+# Disable unnecessary services
+services_to_disable=(
+    "bluetooth"
+    "cups"
+    "avahi-daemon"
+    "iscsi"
+    "autofs"
+    "rpcbind"
+    "postfix"
+    # Add more services to disable as needed
+)
 
-# Set user and group ownership on sensitive files
-chown root:root /etc/passwd
-chown root:root /etc/shadow
-chown root:root /etc/group
-chown root:root /etc/gshadow
+for service in "${services_to_disable[@]}"; do
+    systemctl disable "$service"
+    systemctl stop "$service"
+done
 
-# Set proper permissions on sensitive files
-chmod 644 /etc/passwd
-chmod 400 /etc/shadow
-chmod 644 /etc/group
-chmod 400 /etc/gshadow
-
-# Configure password policy
+# Set strong password policies
 authconfig --passminlen=12 --passminclass=3 --update
 
-# Lock accounts with no passwords
-passwd -l $(awk -F: '($2 == "") {print $1}' /etc/shadow)
+# Lock accounts with no password
+awk -F: '($2 == "") {print $1}' /etc/shadow | while read -r user; do
+    passwd -l "$user"
+done
 
 # Set password expiration policy
 chage -M 90 -m 7 -W 7 root
 
-# Install and configure SELinux
-yum install -y selinux-policy-targeted
+# Enable SELinux
 setenforce 1
+yum install -y selinux-policy-targeted
 
-# Update the system
-yum update -y
+# Configure firewall
+firewall-cmd --permanent --zone=public --remove-service=dhcpv6-client
+firewall-cmd --permanent --add-service=ssh
+firewall-cmd --reload
 
-# Enable automatic security updates
-yum install -y yum-cron
-systemctl enable yum-cron
-systemctl start yum-cron
+# SSH hardening (disable root login, password authentication, etc.)
+# Capture the current username
+CURRENT_USER=$(whoami)
 
-# Configure SSH to use key-based authentication
-#sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
-#systemctl restart sshd
-echo "Create a normal user, disabling root login.."
-# Disable root login via SSH
-sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+# Configure SSH server for maximum security
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+echo "PermitEmptyPasswords no" >> /etc/ssh/sshd_config
+echo "UsePAM no" >> /etc/ssh/sshd_config
+echo "AllowUsers $CURRENT_USER" >> /etc/ssh/sshd_config
+
+# Use SSH key-based authentication
+mkdir -p /home/$CURRENT_USER/.ssh
+chmod 700 /home/$CURRENT_USER/.ssh
+ssh-keygen -t ed25519 -N 'your_passphrase' -f /home/$CURRENT_USER/.ssh/id_ed25519
+
+# Append the contents of id_ed25519.pub to authorized_keys
+cat /home/$CURRENT_USER/.ssh/id_ed25519.pub >> /home/$CURRENT_USER/.ssh/authorized_keys
+chmod 600 /home/$CURRENT_USER/.ssh/authorized_keys
+
+# Set a strong SSH key passphrase
+# (If you haven't set a passphrase earlier)
+# ssh-keygen -p -N 'your_passphrase' -f /home/$CURRENT_USER/.ssh/id_ed25519
+
+# Restart SSH service to apply changes
 systemctl restart sshd
 
-# Configure sudo access
-echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers.d/wheel
+
+
 
 # Disable IPv6
 echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
@@ -78,5 +94,46 @@ echo "install sctp /bin/true" >> /etc/modprobe.d/CIS.conf
 echo "install rds /bin/true" >> /etc/modprobe.d/CIS.conf
 echo "install tipc /bin/true" >> /etc/modprobe.d/CIS.conf
 
-# Reboot the system to apply changes
-reboot
+# Enable automatic security updates
+yum install -y yum-cron
+systemctl enable yum-cron
+
+# Set up a GRUB password
+echo " Set up a GRUB password "
+grub2-setpassword
+
+# Lock down GRUB editing
+echo "GRUB_DISABLE_RECOVERY='true'" >> /etc/default/grub
+grub2-mkconfig -o /boot/grub2/grub.cfg
+
+# Secure /tmp directory
+echo "none /tmp tmpfs defaults,rw,nosuid,nodev,noexec,relatime" >> /etc/fstab
+
+# Limit core dumps
+echo "* hard core 0" >> /etc/security/limits.conf
+
+# Install and configure intrusion detection system (e.g., rkhunter)
+yum install -y rkhunter
+rkhunter --propupd
+
+# Set SSH banner message
+echo "Banner /etc/issue.net" >> /etc/ssh/sshd_config
+
+# Regularly review and rotate logs (use logrotate)
+
+# Implement 2FA for critical services
+
+# Regularly scan for malware (e.g., ClamAV)
+
+# Secure GRUB bootloader configuration (set a GRUB password)
+
+# Implement network segmentation and firewall rules as needed
+
+# Consider additional security measures, such as file integrity monitoring
+
+# Implement incident response and security audit procedures
+
+# Schedule regular backups and test restores
+
+echo "Red Hat Linux hardening complete."
+
